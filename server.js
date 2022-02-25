68b20d68d8b320aa5f46af61ebc9ae4ddb642a7d
CloudyLondon <jovialcty@gmail.com>
2022-02-25 15:06:21 -0800

    add routes

--- server.js
+++ server.js
@@ -3,19 +3,14 @@ const PORT = 3001;
 const app = express();
 const path = require("path");
 
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
 //they are middleware functions
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/notes.html"));
-});
-
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
+  console.log(`API server now listening on ${PORT}`);
 });

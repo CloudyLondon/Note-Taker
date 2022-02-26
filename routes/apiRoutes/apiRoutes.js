f7c970176aad62c527c7ea30feeb67254746006b
CloudyLondon <jovialcty@gmail.com>
2022-02-25 16:24:11 -0800

    add lib

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,9 +1,20 @@
 const path = require("path");
 const router = require("express").Router();
-const data = require("../..db.json");
+const data = require("../../db/db.json");
+const { readFile, createNote } = require("../../lib/notes");
 
-router.get("/api/notes", (req, res) => {});
+router.get("/notes", (req, res) => {
+  const { notes } = readFile();
+  const results = notes;
+  res.json(results);
+});
 
-router.post();
+router.post("/notes", (req, res) => {
+  const { notes } = readFile(); //
+  const newNote = createNote(body, notes);
+  res.json(newNote);
+});
+
+// router.delete("/notes", (req, res) => {});
 
 module.exports = router;
